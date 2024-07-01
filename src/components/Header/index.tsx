e95c87331e6a4dd639000d3501ba83f60834af15
Jales Nunes <jales.nunes@hotmail.com>
2024-07-01 16:48:25 -0600

    media queries corrections to fit on mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,13 @@
-import logoImg from '../../assets/logo.svg'
-import leftEffectHeader from '../../assets/leftEffectHeader.svg'
-import rightEffectHeader from '../../assets/rightEffectHeader.svg'
+import logoImg from "../../assets/logo.svg";
+import leftEffectHeader from "../../assets/leftEffectHeader.svg";
+import rightEffectHeader from "../../assets/rightEffectHeader.svg";
 
 export function Header() {
-    return(
-        <div className='h-72 flex justify-between items-center bg-baseProfile relative z-0 max-sm:w-fit'>
-            <img className='z-0 max-sm:w-[12rem]' src={leftEffectHeader} alt="" />
-            <img className='max-sm:m-auto' src={logoImg} alt="" />
-            <img className='z-0 max-sm:w-[12rem]' src={rightEffectHeader} alt="" />
-        </div>
-    )
+  return (
+    <div className="h-72 flex justify-between items-center bg-baseProfile relative z-0">
+      <img className="z-0  max-sm:hidden" src={leftEffectHeader} alt="" />
+      <img className="max-sm:m-auto " src={logoImg} alt="" />
+      <img className="z-0  max-sm:hidden" src={rightEffectHeader} alt="" />
+    </div>
+  );
 }
