b45336f6bdd6715cc7c88a2f0c72ae8e03f3c45b
Jales Nunes <jales.nunes@hotmail.com>
2025-03-13 20:06:58 -0600

    Integrate Axios for GitHub API fetch and display user data on the page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,67 @@
 import { ArrowSquareUpRight, MagnifyingGlass } from "phosphor-react";
+import axios from "axios";
 
 import githubLogo from "../../assets/githubLogo.svg";
 import workplace from "../../assets/workplace.svg";
 import userGroup from "../../assets/userGroup.svg";
 
 import { PostSummary } from "./components/PostSummary/imdex";
+import { useEffect, useState } from "react";
+
+interface GitHubUser {
+  name: string;
+  login: string;
+  avatar_url: string;
+  company: string;
+  followers: number;
+  bio: string;
+}
 
 export function Home() {
+  const [githubUserData, setGithubUserData] = useState<GitHubUser>({
+    name: "",
+    login: "",
+    avatar_url: "",
+    company: "",
+    followers: 0,
+    bio: "",
+  })
+
+  const fetchUserData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.github.com/users/jalesnunes`
+      );
+      setGithubUserData(response.data);
+    } catch (error) {
+      console.error("Erro:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  console.log(githubUserData)
+
   return (
     <main className="w-[54rem] m-auto max-sm:w-fit">
       <section className="bg-baseProfile -mt-16 px-10 py-8 flex flex-row gap-6 items-center justify-between rounded-xl shadow-shadowProfile relative z-10 max-sm:mx-6">
-        <img src={userGroup} alt="" className="w-36 h-36 max-sm:w-16 max-sm:h-16" />
+        <img
+          src={githubUserData.avatar_url}
+          alt=""
+          className="w-36 h-36 max-sm:w-16 max-sm:h-16"
+        />
         <div className="flex flex-col gap-6">
           <header className=" flex items-center justify-between">
-            <h1 className="text-2xl text-baseTitle font-bold max-sm:text-2xl">Jales Nunes</h1>
-            <a href="" className="flex gap-2 hover:border-b  hover:border-b-blue">
+            <h1 className="text-2xl text-baseTitle font-bold max-sm:text-2xl">
+              {githubUserData.name}
+            </h1>
+            <a
+              href="https://github.com/jalesnunes"
+              className="flex gap-2 hover:border-b  hover:border-b-blue"
+              target="_blank"
+            >
               <span className="text-xs text-blue">GITHUB</span>
               <ArrowSquareUpRight size={16} color="#3294f8" />
             </a>
@@ -22,26 +69,30 @@ export function Home() {
 
           <div>
             <p className="text-baseText max-sm:text-sm">
-              Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-              viverra massa quam dignissim aenean malesuada suscipit. Nunc,
-              volutpat pulvinar vel mass.
+              {githubUserData.bio}
             </p>
           </div>
 
           <footer className="flex gap-4">
             <a href="" className="flex gap-2 items-center max-sm:text-xs">
               <img src={githubLogo} alt="" />
-              <span className="text-baseSubtitle hover:text-baseTitle">jalesnunes</span>
+              <span className="text-baseSubtitle hover:text-baseTitle">
+                {githubUserData.login}
+              </span>
             </a>
 
             <a href="" className="flex gap-2 items-center max-sm:text-xs">
               <img src={workplace} alt="" />
-              <span className="text-baseSubtitle hover:text-baseTitle">Somewhere</span>
+              <span className="text-baseSubtitle hover:text-baseTitle">
+                {githubUserData.company}
+              </span>
             </a>
 
             <a href="" className="flex gap-2 items-center max-sm:text-xs">
               <img src={userGroup} alt="" />
-              <span className="text-baseSubtitle hover:text-baseTitle">10 Followers</span>
+              <span className="text-baseSubtitle hover:text-baseTitle">
+                {githubUserData.followers} Followers
+              </span>
             </a>
           </footer>
         </div>
@@ -58,7 +109,10 @@ export function Home() {
             type="text"
             placeholder="Search publications"
           />
-          <button className="flex items-center gap-3 p-4 rounded-md font-bold bg-baseProfile border-baseBorder border text-baseTitle transition-colors hover:bg-basePost" type="submit">
+          <button
+            className="flex items-center gap-3 p-4 rounded-md font-bold bg-baseProfile border-baseBorder border text-baseTitle transition-colors hover:bg-basePost"
+            type="submit"
+          >
             <MagnifyingGlass size={20} />
             Search
           </button>
@@ -74,3 +128,4 @@ export function Home() {
     </main>
   );
 }
+
